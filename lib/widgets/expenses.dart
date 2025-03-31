b18a21fbb7b677f274903f527ec7719c767910f1
Sujal Acharya <sujalacharya567@gmail.com>
2025-03-31 01:25:09 -0300

    Learning new things

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -29,7 +30,13 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [Text("The Chart"), Text('Expense List...')]),
+      body: Column(
+        children: [
+          Text("The Chart"),
+          Text('Expense List...'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
     );
   }
 }
