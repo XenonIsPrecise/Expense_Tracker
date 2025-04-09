1d2d41c92b609bcaf0baa6a1408733553b0a32cb
Sujal Acharya <sujalacharya567@gmail.com>
2025-04-09 13:37:31 -0300

    Dropdown added

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -19,6 +19,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -87,6 +88,18 @@ class _NewExpenseState extends State<NewExpense> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              DropdownButton(
+                items:
+                    Category.values
+                        .map(
+                          (category) => DropdownMenuItem(
+                            value: category,
+                            child: Text(category.name.toUpperCase()),
+                          ),
+                        )
+                        .toList(),
+                onChanged: (value) {},
+              ),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
