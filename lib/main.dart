0503ba4ff2b053139d78f5daf4d6486fd9a04a32
Sujal Acharya <sujalacharya567@gmail.com>
2025-04-10 21:48:43 -0300

    Learning theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 58, 211, 198),
+);
+
 void main() {
-  runApp(MaterialApp(home: Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: Expenses(),
+    ),
+  );
 }
